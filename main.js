0971ea132e7e0cc1b2ca363ad38ab8e73c877c58
ana-apca <ana.love13@gmail.com>
2020-02-13 17:31:07 -0300

    Inserção Banner e citações

---
+++ main.js
@@ -0,0 +1,125 @@
+
+/*
+function primo(num) {
+    for (var i = 2; i < num; i++)
+    if (num % i === 0) return false;
+    return num !== 1;
+}
+   // var resl = "O n�mero" + primo(num) + "� primo"}
+function  retornaResposta(num2){
+    if (primo(num2)){
+        return "o numero " + num2 + " � primo "
+    }else{
+        return "o numero " + num2 + " n�o � primo "
+    }
+}  
+
+var num1 = prompt(" Informe um numero 123" )
+console.log (retornaResposta(num1));
+*/
+
+/*
+function botao(){  
+    document.getElementById("goodJob").innerHTML = " vc fez um bom trabalho";
+}
+
+function outraPagina1(){
+    window.open("https://www.globo.com/");
+    //window.location.href = "https://www.globo.com/";
+}
+
+function mouseAqui(){
+    document.getElementById("mexerMouse").innerHTML = "fez certinho"
+    //alert("escreva de novo")
+}
+
+function volte(){
+    document.getElementById("mexerMouse").innerHTML = "refa�a"
+}
+
+function mouseAqui(elemento){
+        elemento.innerHTML = "fa�a de novo"
+        //document.getElementById("mexerMouse").innerHTML = "fez certinho"
+        //alert("escreva de novo")
+    }
+*/
+
+// var lista = [" uva ",  " kiwi ", " banana "];
+//lista.push(" pera ");
+//lista.pop();
+/*console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - "));
+
+//var idade = 37;
+//var frase = "bebe não deixa eu fazer as coisas sozinha";
+//alert(nome + " tem " + idade + " anos ");
+//console.log(frase);
+*/
+
+/* var fruta = {nome: "uva ", cor: "verde"}    
+console.log(fruta.nome);
+alert(fruta.cor); */
+
+/*var frutas = [{nome: "uva ", cor: "verde"}, {nome: "maça ", cor: "vermelha"}];   
+console.log(frutas);
+alert(frutas[1].cor);*/
+
+/*var idade = prompt("Qual sua idade");
+//var idade  = 18;
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++
+}
+*/
+
+/*var count
+for(count=0; count <=5; count++){
+    alert(count);
+}
+*/
+
+/*var d = new Date();
+//alert(d.getMonth()+1);
+alert(d.getMinutes());
+*/
+
+/*function soma(n1, n2){
+    return n1 + n2;
+}
+alert(soma(5,10));
+
+/*function setReplace (frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+    alert(setReplace("Vai Ana", "Paula", "Araujo"));
+*/
+
+/*function validaIdade(idade){
+    var validar;
+    if (idade >=18){
+        validar = true
+           }else{
+        validar = false
+           }
+    return validar;
+}
+
+var idade = prompt ("qual sua idade")
+console.log(validaIdade(idade))
+*/
+
+
+
+
+
+
